899ea10f828e16000070fd21465e8032721ce04a
Mitsunori Komatsu <komamitsu@gmail.com>
2014-12-16 01:44:27 +0900

    Refactor test code and add some tests

--- TreasureDataTests/TreasureDataTests.m
+++ TreasureDataTests/TreasureDataTests.m
@@ -11,18 +11,11 @@
 #import "TDClient.h"
 
 static NSString *END_POINT = @"http://localhost";
-@interface TreasureDataTests : XCTestCase
-@property bool isFinished;
-@end
-
 
 @interface MyTDClient : TDClient
 @property NSURLRequest *requestData;
 @property NSData *expectedResponseBody;
 @property NSURLResponse *expectedResponse;
-@end
-
-@interface MyTDClient ()
 @end
 
 @implementation MyTDClient
@@ -45,10 +38,20 @@ static NSString *END_POINT = @"http://localhost";
 }
 @end
 
+@interface TreasureDataTests : XCTestCase
+@property bool isFinished;
+@property MyTreasureData* td;
+@property MyTDClient *client;
+@end
+
 @implementation TreasureDataTests
 
 - (void)setUp
 {
+    self.td = [[MyTreasureData alloc] initWithApiKey:@"dummy_apikey"];
+    self.client = (MyTDClient*)self.td.client;
+    [[MyTDClient getEventStore] deleteAllEvents];
+    [MyTreasureData disableEventCompression];
     [super setUp];
 }
 
@@ -60,38 +63,96 @@ static NSString *END_POINT = @"http://localhost";
     [super tearDown];
 }
 
-- (void)testSingleEvent {
-    MyTreasureData *td = [[MyTreasureData alloc] initWithApiKey:@"dummy_apikey"];
-    [[MyTDClient getEventStore] deleteAllEvents];
-    MyTDClient* client = (MyTDClient*)td.client;
-    [MyTreasureData disableEventCompression];
-    
+- (void)setupDefaultExpectedResponse {
     NSHTTPURLResponse *expectedResponse = [[NSHTTPURLResponse alloc] initWithURL:nil statusCode:200 HTTPVersion:@"1.1" headerFields:nil];
-    client.expectedResponse = expectedResponse;
-    
-    NSError *error = [NSError alloc];
-    NSData *expectedResponseBody = [NSJSONSerialization dataWithJSONObject:@{@"db_.tbl":@[@{@"success":@"true"}]} options:0 error:&error];
-    client.expectedResponseBody = expectedResponseBody;
-    
-    [td addEvent:@{@"name":@"foobar"} database:@"db_" table:@"tbl" ];
-    [td uploadEventsWithCallback:^(){
-        NSString *url = [client.requestData.URL absoluteString];
-        XCTAssertTrue([@"http://localhost/ios/v3/event" isEqualToString:url]);
-        NSError *error = [NSError alloc];
-        NSDictionary *ev = [NSJSONSerialization JSONObjectWithData:client.requestData.HTTPBody options:0 error:&error];
-        XCTAssertEqual(1, ev.count);
-        NSArray *arr = [ev objectForKey:@"db_.tbl"];
-        XCTAssertEqual(1, arr.count);
-        NSDictionary *dict = [arr objectAtIndex:0];
-        XCTAssertTrue([[dict objectForKey:@"name"] isEqualToString:@"foobar"]);
-        self.isFinished = true;
-    }
-                          onError:^(NSString* ecode, NSString* detail){
-                              XCTAssertTrue(false);
-                              self.isFinished = true;
-                          }
-     ];
+    self.client.expectedResponse = expectedResponse;
 }
 
+- (void)setupDefaultExpectedResponseBody:(NSDictionary*)dict {
+    NSError *error = [NSError alloc];
+    NSData *expectedResponseBody = [NSJSONSerialization dataWithJSONObject:dict options:0 error:&error];
+    self.client.expectedResponseBody = expectedResponseBody;
+}
+
+- (void)assertRequest:(void(^)(NSDictionary*))assertion {
+    NSString *url = [self.client.requestData.URL absoluteString];
+    XCTAssertTrue([@"http://localhost/ios/v3/event" isEqualToString:url]);
+    NSError *error = [NSError alloc];
+    NSDictionary *ev = [NSJSONSerialization JSONObjectWithData:self.client.requestData.HTTPBody options:0 error:&error];
+    assertion(ev);
+}
+
+- (void)baseTesting:(void(^)())setup assertion:(void(^)(NSDictionary*))assert {
+    [self setupDefaultExpectedResponse];
+
+    setup();
+    
+    [self.td uploadEventsWithCallback:^(){
+        [self assertRequest:assert];
+        self.isFinished = true;
+    }
+      onError:^(NSString* ecode, NSString* detail){
+          XCTAssertTrue(false);
+          self.isFinished = true;
+      }];
+}
+
+- (void)testSingleEvent {
+    [self baseTesting:^() {
+        [self setupDefaultExpectedResponseBody: @{@"db_.tbl":@[@{@"success":@"true"}]}];
+        [self.td addEvent:@{@"name":@"foobar"} database:@"db_" table:@"tbl"];
+    }
+        assertion:^(NSDictionary *ev){
+            XCTAssertEqual(1, ev.count);
+            NSArray *arr = [ev objectForKey:@"db_.tbl"];
+            XCTAssertEqual(1, arr.count);
+            NSDictionary *dict = [arr objectAtIndex:0];
+            XCTAssertTrue([[dict objectForKey:@"name"] isEqualToString:@"foobar"]);
+        }];
+}
+
+- (void)testSingleEventWithDefaultDatabase {
+    [self baseTesting:^() {
+        [self.td setDefaultDatabase:@"db_"];
+        [self setupDefaultExpectedResponseBody: @{@"db_.tbl":@[@{@"success":@"true"}]}];
+        [self.td addEvent:@{@"name":@"foobar"} table:@"tbl"];
+    }
+            assertion:^(NSDictionary *ev){
+                XCTAssertEqual(1, ev.count);
+                NSArray *arr = [ev objectForKey:@"db_.tbl"];
+                XCTAssertEqual(1, arr.count);
+                NSDictionary *dict = [arr objectAtIndex:0];
+                XCTAssertTrue([[dict objectForKey:@"name"] isEqualToString:@"foobar"]);
+            }];
+}
+
+- (void)testMultiEvents {
+    [self baseTesting:^() {
+        [self setupDefaultExpectedResponseBody:
+            @{
+              @"db0.tbl0":@[@{@"success":@"true"}, @{@"success":@"true"}],
+              @"db1.tbl1":@[@{@"success":@"true"}]
+            }
+        ];
+        [self.td addEvent:@{@"name":@"one"} database:@"db0" table:@"tbl0"];
+        [self.td addEvent:@{@"name":@"two"} database:@"db1" table:@"tbl1"];
+        [self.td addEvent:@{@"name":@"three"} database:@"db0" table:@"tbl0"];
+    }
+            assertion:^(NSDictionary *ev){
+                XCTAssertEqual(2, ev.count);
+                
+                NSArray *arr = [ev objectForKey:@"db0.tbl0"];
+                XCTAssertEqual(2, arr.count);
+                NSDictionary *dict = [arr objectAtIndex:0];
+                XCTAssertTrue([[dict objectForKey:@"name"] isEqualToString:@"one"]);
+                dict = [arr objectAtIndex:1];
+                XCTAssertTrue([[dict objectForKey:@"name"] isEqualToString:@"three"]);
+                
+                arr = [ev objectForKey:@"db1.tbl1"];
+                XCTAssertEqual(1, arr.count);
+                dict = [arr objectAtIndex:0];
+                XCTAssertTrue([[dict objectForKey:@"name"] isEqualToString:@"two"]);
+            }];
+}
 
 @end
