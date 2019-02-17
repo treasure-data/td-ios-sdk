ded4420e897951bc9a57032a6116d8967fef0d01
huylenq <huy.lenq@gmail.com>
2019-02-18 00:38:56 +0700

    Add integration test cases for 3 basic flows
    - Custom events
    - App lifecycle events
    - Session

---
+++ TreasureDataTests/IntegrationTests.swift
@@ -0,0 +1,120 @@
+//
+//  TreasureDataSwiftTests.swift
+//  TreasureDataTests
+//
+//  Created by Huy TD on 1/27/19.
+//  Copyright © 2019 Huy TD. All rights reserved.
+//
+
+import XCTest
+
+class IntegrationTests: XCTestCase {
+
+    var sdkClient: TreasureData!
+    static var api: TDAPI!
+    static var apiKey: String!
+
+    static var sessionPrefix = uuid()
+    static var targetDatabase = "ios_integration_tests"
+
+    /** Tables used in a single test */
+    var tempTables: [String] = []
+
+    override class func setUp() {
+        apiKey = ProcessInfo.processInfo.environment["TD_DEV_MASTER_KEY"]
+        api = TDAPI.init(endpoint: "https://api-development.treasuredata.com", apiKey: apiKey)
+        if (!(try! api.isDatabaseExist(targetDatabase))) {
+            fatalError("Either the apiKey is invalid or the target database \(targetDatabase) is not exist!")
+        }
+    }
+
+    override func setUp() {
+        TreasureData.initializeApiEndpoint("https://in-development.treasuredata.com")
+        sdkClient = TreasureData(apiKey: IntegrationTests.apiKey)
+        sdkClient.defaultDatabase = IntegrationTests.targetDatabase
+        tempTables = []
+    }
+
+    override func tearDown() {
+        let remoteTables = Set<String>(IntegrationTests.api.listTables(database: IntegrationTests.targetDatabase))
+        for sessionTable in tempTables {
+            if remoteTables.contains(sessionTable) {
+                try! IntegrationTests.api.deleteTable(database: IntegrationTests.targetDatabase, table: sessionTable)
+            } else {
+                print("WARN: Skip deleting table '\(IntegrationTests.targetDatabase).\(sessionTable)' as it is not exist!")
+            }
+        }
+        sdkClient.disableAppLifecycleEvent()
+        sdkClient = nil
+        IntegrationTests.api = nil
+    }
+
+    func testCustomEvent() {
+        sdkClient.enableCustomEvent()
+        let table = newTempTable()
+        let event = sdkClient.addEvent(["message": "Guten Morgen!"], table: table)
+        sdkClient.uploadEvents()
+        if event != nil {
+            let result = try! IntegrationTests.api.stubbornQuery("select message from \(table) limit 1", database: "huy")
+            XCTAssert(result[0][0] as! String == "Guten Morgen!")
+        } else {
+            XCTFail("Could not create event!")
+        }
+    }
+
+    func testLifeCycleAppOpenedEvent() {
+        sdkClient.enableAppLifecycleEvent()
+        sdkClient.defaultTable = newTempTable()
+        NotificationCenter.default.post(name: Notification.Name("UIApplicationDidFinishLaunchingNotification"), object: nil)
+        sdkClient.uploadEvents()
+        let result = try! IntegrationTests.api.stubbornQuery(
+            "select td_ios_event, td_app_ver, td_app_ver_num from \(sdkClient.defaultTable!) limit 1",
+            database: IntegrationTests.targetDatabase)
+        XCTAssert(result[0][0] as! String == "TD_IOS_APP_OPEN")
+        XCTAssertNotNil(result[0][1])
+        XCTAssertNotNil(result[0][2])
+    }
+
+    func testSession() {
+        let sessionTable = newTempTable()
+        let eventTable = newTempTable()
+
+        sdkClient.startSession(sessionTable)
+        let sessionId: String = sdkClient.getSessionId()!
+
+        sdkClient.addEvent(["event": "in_session_event"], table: eventTable)
+        sdkClient.endSession(sessionTable)
+        sdkClient.addEvent(["event": "not_in_session_event"], table: eventTable)
+        sdkClient.uploadEvents()
+
+        let sessionRecords = try! IntegrationTests.api.stubbornQuery(
+            "select td_session_event, td_session_id from \(sessionTable)",
+            database: IntegrationTests.targetDatabase)
+        let sessionEvents: Set = [sessionRecords[0][0] as! String, sessionRecords[1][0] as! String]
+        XCTAssertTrue(sessionEvents.contains("start"))
+        XCTAssertTrue(sessionEvents.contains("end"))
+        XCTAssertEqual(sessionRecords[0][1] as! String, sessionId)
+        XCTAssertEqual(sessionRecords[1][1] as! String, sessionId)
+
+        let eventRecords = try! IntegrationTests.api.stubbornQuery(
+            "select td_session_id, event from \(eventTable)",
+            database: IntegrationTests.targetDatabase)
+        for event in eventRecords {
+            if (event[1] as! String == "in_session_event") {
+                XCTAssertEqual(event[0] as? String, sessionId)
+            } else if (event[1] as! String == "not_in_session_event") {
+                XCTAssertEqual(event[0] as! String, "")
+            }
+        }
+    }
+
+    private func newTempTable() -> String {
+        let table: String = "ios_integration_test_" + NSUUID().uuidString.lowercased().replacingOccurrences(of: "-", with: "_")
+        tempTables.append(table)
+        return table
+    }
+
+    static func uuid() -> String {
+        return NSUUID().uuidString.lowercased().replacingOccurrences(of: "-", with: "_")
+    }
+}
