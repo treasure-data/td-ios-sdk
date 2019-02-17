c93999efcc519c6b49227c4cdd46462b58f67fc3
huylenq <huy.lenq@gmail.com>
2019-02-18 00:34:42 +0700

    Add integration tests

---
+++ TestHost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestHost
+//
+//  Created by Huy TD on 1/28/19.
+//  Copyright © 2019 Huy TD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
