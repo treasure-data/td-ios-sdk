464e19ab651f9e6346007a6491440dea93c333ff
huylenq <huy.lenq@gmail.com>
2019-02-26 10:31:47 +0700

    Switch default integration tests' target database

--- TreasureDataTests/IntegrationTests.swift
+++ TreasureDataTests/IntegrationTests.swift
@@ -14,8 +14,8 @@ class IntegrationTests: XCTestCase {
     static var api: TDAPI!
     static var apiKey: String!
 
+    static let TargetDatabase = "ios_it"
     static var sessionPrefix = uuid()
-    static var targetDatabase = "ios_integration_tests"
 
     /** Tables used in a single test */
     var tempTables: [String] = []
@@ -23,30 +23,33 @@ class IntegrationTests: XCTestCase {
     override class func setUp() {
         apiKey = ProcessInfo.processInfo.environment["TD_DEV_MASTER_KEY"]
         api = TDAPI.init(endpoint: "https://api-development.treasuredata.com", apiKey: apiKey)
-        if (!(try! api.isDatabaseExist(targetDatabase))) {
-            fatalError("Either the apiKey is invalid or the target database \(targetDatabase) is not exist!")
+        if (!(try! api.isDatabaseExist(TargetDatabase))) {
+            fatalError("Either the apiKey is invalid or the target database \(TargetDatabase) is not exist!")
         }
+    }
+
+    override class func tearDown() {
+        IntegrationTests.api = nil
     }
 
     override func setUp() {
         TreasureData.initializeApiEndpoint("https://in-development.treasuredata.com")
         sdkClient = TreasureData(apiKey: IntegrationTests.apiKey)
-        sdkClient.defaultDatabase = IntegrationTests.targetDatabase
+        sdkClient.defaultDatabase = IntegrationTests.TargetDatabase
         tempTables = []
     }
 
     override func tearDown() {
-        let remoteTables = Set<String>(IntegrationTests.api.listTables(database: IntegrationTests.targetDatabase))
+        let remoteTables = Set<String>(IntegrationTests.api.listTables(database: IntegrationTests.TargetDatabase))
         for sessionTable in tempTables {
             if remoteTables.contains(sessionTable) {
-                try! IntegrationTests.api.deleteTable(database: IntegrationTests.targetDatabase, table: sessionTable)
+                try! IntegrationTests.api.deleteTable(database: IntegrationTests.TargetDatabase, table: sessionTable)
             } else {
-                print("WARN: Skip deleting table '\(IntegrationTests.targetDatabase).\(sessionTable)' as it is not exist!")
+                print("WARN: Skip deleting table '\(IntegrationTests.TargetDatabase).\(sessionTable)' as it is not exist!")
             }
         }
         sdkClient.disableAppLifecycleEvent()
         sdkClient = nil
-        IntegrationTests.api = nil
     }
 
     func testCustomEvent() {
@@ -55,7 +58,7 @@ class IntegrationTests: XCTestCase {
         let event = sdkClient.addEvent(["message": "Guten Morgen!"], table: table)
         sdkClient.uploadEvents()
         if event != nil {
-            let result = try! IntegrationTests.api.stubbornQuery("select message from \(table) limit 1", database: "huy")
+            let result = try! IntegrationTests.api.stubbornQuery("select message from \(table) limit 1", database: IntegrationTests.TargetDatabase)
             XCTAssert(result[0][0] as! String == "Guten Morgen!")
         } else {
             XCTFail("Could not create event!")
@@ -69,7 +72,7 @@ class IntegrationTests: XCTestCase {
         sdkClient.uploadEvents()
         let result = try! IntegrationTests.api.stubbornQuery(
             "select td_ios_event, td_app_ver, td_app_ver_num from \(sdkClient.defaultTable!) limit 1",
-            database: IntegrationTests.targetDatabase)
+            database: IntegrationTests.TargetDatabase)
         XCTAssert(result[0][0] as! String == "TD_IOS_APP_OPEN")
         XCTAssertNotNil(result[0][1])
         XCTAssertNotNil(result[0][2])
@@ -89,7 +92,7 @@ class IntegrationTests: XCTestCase {
 
         let sessionRecords = try! IntegrationTests.api.stubbornQuery(
             "select td_session_event, td_session_id from \(sessionTable)",
-            database: IntegrationTests.targetDatabase)
+            database: IntegrationTests.TargetDatabase)
         let sessionEvents: Set = [sessionRecords[0][0] as! String, sessionRecords[1][0] as! String]
         XCTAssertTrue(sessionEvents.contains("start"))
         XCTAssertTrue(sessionEvents.contains("end"))
@@ -98,7 +101,7 @@ class IntegrationTests: XCTestCase {
 
         let eventRecords = try! IntegrationTests.api.stubbornQuery(
             "select td_session_id, event from \(eventTable)",
-            database: IntegrationTests.targetDatabase)
+            database: IntegrationTests.TargetDatabase)
         for event in eventRecords {
             if (event[1] as! String == "in_session_event") {
                 XCTAssertEqual(event[0] as? String, sessionId)
