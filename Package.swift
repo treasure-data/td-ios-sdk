0c7534eba989d626eea00b8d1891b8dbd8182281
Thanh Tung Vu <tung.vu@treasure-data.com>
2022-10-05 16:34:53 +0700

    Missing ,

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["TreasureData_iOS_SDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/treasure-data/KeenClient-iOS.git", .exact("3.9.0"))
+        .package(url: "https://github.com/treasure-data/KeenClient-iOS.git", .exact("3.9.0")),
         .package(url: "https://github.com/nicklockwood/GZIP.git", .upToNextMajor("1.3.0"))
     ],
     targets: [
